5c0ad85252e3b215550d18cb4df7ef767a32cd99
Marcus Xavier <marcus.xavier@appmax.com.br>
2022-11-07 23:13:46 -0300

    adicionando cores

--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,22 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   </React.StrictMode>
 )
 
-
 function setBackgroundVar() {
-  const colors = ['#258a40', '#231a6e', '#921196', 'pink', '#383380', '#138f67'];
+  const colors = [
+    '#254441',
+    '#231a6e',
+    '#43AA8B',
+    '#E56B70',
+    '#383380',
+    '#138f67',
+    '#c46bae',
+    '#310A31',
+    '#4CB944',
+    '#B80C09',
+    '#885A89',
+    '#8B80F9',
+    '#B6244F',
+  ]
   const randomColor = colors[Math.floor(Math.random() * colors.length)]
   const root = document.querySelector(':root')
   //@ts-ignore
